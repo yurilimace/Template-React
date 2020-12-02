5cd80f1c4612f146f593f6d2ec627eea65b717e8
José Yuri <yurilima.ce@gmail.com>
2020-12-01 23:05:39 -0300

    Estudo do template e estilização do menu.

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -4,14 +4,11 @@ import CIcon from '@coreui/icons-react'
 export default [
   {
     _tag: 'CSidebarNavItem',
-    name: 'Dashboard',
+    name: 'CSP Logo',
     to: '/dashboard',
     icon: <CIcon name="cil-speedometer" customClasses="c-sidebar-nav-icon"/>,
-    badge: {
-      color: 'info',
-      text: 'NEW',
-    }
   },
+
   {
     _tag: 'CSidebarNavTitle',
     _children: ['Theme']
